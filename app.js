0fbc232257f41bd2cf9dbaddd81069166f7fe8e9
Slava Knyazev <slava@knyz.org>
2016-07-29 00:31:32 -0400

    fixed https bug

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var upload = multer({ dest: 'uploads/' })
 var app = express()
 
 app.get('/', function (req, res) {
-  res.send('<script src="https://code.jquery.com/jquery-3.1.0.min.js"> </script><script src="http://malsup.github.com/jquery.form.js"></script><form method="POST" enctype="multipart/form-data"><input name="file" type="file"> <input type="submit"></form><script>$("form").ajaxForm(function(result) {alert("File size: " + JSON.parse(result).size);});</script>');
+  res.send('<script src="https://code.jquery.com/jquery-3.1.0.min.js"> </script><script src="https://malsup.github.com/jquery.form.js"></script><form method="POST" enctype="multipart/form-data"><input name="file" type="file"> <input type="submit"></form><script>$("form").ajaxForm(function(result) {alert("File size: " + JSON.parse(result).size);});</script>');
 });
 app.post('/', upload.single('file'), function (req, res) {
   res.send(JSON.stringify(req.file));
